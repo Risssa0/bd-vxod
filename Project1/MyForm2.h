4498d7c34ff3840e021029814ee3a8378297e8a7
Ivan Baschanov <ldpsdsklkd23@mail.ru>
2024-04-17 12:29:18 +0300

    add Search ( доделать/ work/ криво  )

--- Project1/MyForm2.h
+++ Project1/MyForm2.h
@@ -2,6 +2,7 @@
 #include <sqlite3.h>
 #include "MyForm1.h"
 #include "MyForm3.h"
+#include "MyForm4.h"
 namespace Project1 {
 
 	using namespace System;
@@ -41,6 +42,7 @@ namespace Project1 {
 	private: System::Windows::Forms::Label^ label1;
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
 
 	private:
 		/// <summary>
@@ -59,6 +61,7 @@ namespace Project1 {
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->panel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -109,12 +112,26 @@ namespace Project1 {
 			this->button2->UseVisualStyleBackColor = false;
 			this->button2->Click += gcnew System::EventHandler(this, &MyForm2::button2_Click);
 			// 
+			// button3
+			// 
+			this->button3->BackColor = System::Drawing::Color::BlueViolet;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 9.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button3->Location = System::Drawing::Point(342, 151);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(141, 39);
+			this->button3->TabIndex = 3;
+			this->button3->Text = L"Поиск даных";
+			this->button3->UseVisualStyleBackColor = false;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm2::button3_Click);
+			// 
 			// MyForm2
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ButtonShadow;
 			this->ClientSize = System::Drawing::Size(495, 419);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->panel1);
@@ -136,6 +153,12 @@ namespace Project1 {
 	}
 private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 	MyForm3^ f1 = gcnew MyForm3();
+	f1->Owner = this;
+	f1->Show();
+	this->Hide();
+}
+private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	MyForm4^ f1 = gcnew MyForm4();
 	f1->Owner = this;
 	f1->Show();
 	this->Hide();
--- Project1/MyForm4.h
+++ Project1/MyForm4.h
@@ -4,7 +4,7 @@
 #include <string>
 #include <msclr/marshal_cppstd.h>
 namespace Project1 {
-	using namespace System::Runtime::InteropServices;
+
 	using namespace System;
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
@@ -13,12 +13,12 @@ namespace Project1 {
 	using namespace System::Drawing;
 
 	/// <summary>
-	/// Сводка для MyForm3
+	/// Сводка для MyForm4
 	/// </summary>
-	public ref class MyForm3 : public System::Windows::Forms::Form
+	public ref class MyForm4 : public System::Windows::Forms::Form
 	{
 	public:
-		MyForm3(void)
+		MyForm4(void)
 		{
 			InitializeComponent();
 			//
@@ -30,7 +30,7 @@ namespace Project1 {
 		/// <summary>
 		/// Освободить все используемые ресурсы.
 		/// </summary>
-		~MyForm3()
+		~MyForm4()
 		{
 			if (components)
 			{
@@ -39,18 +39,41 @@ namespace Project1 {
 		}
 	private: System::Windows::Forms::Panel^ panel1;
 	protected:
-	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+
 	private: System::Windows::Forms::Label^ label3;
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::Button^ button1;
 	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ PersonID;
+	private: System::Windows::Forms::DataGridViewTextBoxColumn^ name;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	private:
 		/// <summary>
 		/// Обязательная переменная конструктора.
 		/// </summary>
-		System::ComponentModel::Container^ components;
+		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -66,7 +89,11 @@ namespace Project1 {
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->PersonID = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			this->name = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			this->panel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// panel1
@@ -74,23 +101,22 @@ namespace Project1 {
 			this->panel1->BackColor = System::Drawing::Color::BlueViolet;
 			this->panel1->Controls->Add(this->label2);
 			this->panel1->Controls->Add(this->label1);
-			this->panel1->Location = System::Drawing::Point(-45, -6);
+			this->panel1->Location = System::Drawing::Point(-44, -7);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(583, 110);
-			this->panel1->TabIndex = 1;
-			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm3::panel1_Paint);
+			this->panel1->Size = System::Drawing::Size(583, 148);
+			this->panel1->TabIndex = 2;
 			// 
 			// label2
 			// 
 			this->label2->AutoSize = true;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 20.25F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(204)));
-			this->label2->Location = System::Drawing::Point(121, 40);
+			this->label2->Location = System::Drawing::Point(154, 53);
 			this->label2->Name = L"label2";
-			this->label2->Size = System::Drawing::Size(395, 31);
+			this->label2->Size = System::Drawing::Size(347, 31);
 			this->label2->TabIndex = 1;
-			this->label2->Text = L"Удаление в таблице: Person";
-			this->label2->Click += gcnew System::EventHandler(this, &MyForm3::label2_Click);
+			this->label2->Text = L"Поиск в таблице: Person";
+			this->label2->Click += gcnew System::EventHandler(this, &MyForm4::label2_Click);
 			// 
 			// label1
 			// 
@@ -102,65 +128,92 @@ namespace Project1 {
 			// label3
 			// 
 			this->label3->AutoSize = true;
-			this->label3->Location = System::Drawing::Point(12, 138);
+			this->label3->Location = System::Drawing::Point(12, 163);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(18, 13);
-			this->label3->TabIndex = 10;
+			this->label3->TabIndex = 11;
 			this->label3->Text = L"ID";
 			// 
 			// textBox1
 			// 
-			this->textBox1->Location = System::Drawing::Point(48, 135);
+			this->textBox1->Location = System::Drawing::Point(51, 160);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(148, 20);
-			this->textBox1->TabIndex = 11;
+			this->textBox1->TabIndex = 12;
 			// 
 			// button1
 			// 
 			this->button1->BackColor = System::Drawing::Color::BlueViolet;
-			this->button1->Location = System::Drawing::Point(275, 349);
+			this->button1->Location = System::Drawing::Point(224, 421);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(142, 23);
-			this->button1->TabIndex = 12;
-			this->button1->Text = L"Удалить";
+			this->button1->TabIndex = 13;
+			this->button1->Text = L"Найти";
 			this->button1->UseVisualStyleBackColor = false;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm3::button1_Click);
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm4::button1_Click);
 			// 
 			// button2
 			// 
 			this->button2->BackColor = System::Drawing::Color::BlueViolet;
-			this->button2->Location = System::Drawing::Point(451, 349);
+			this->button2->Location = System::Drawing::Point(435, 421);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(75, 23);
-			this->button2->TabIndex = 16;
+			this->button2->TabIndex = 17;
 			this->button2->Text = L"Назад";
 			this->button2->UseVisualStyleBackColor = false;
-			this->button2->Click += gcnew System::EventHandler(this, &MyForm3::button2_Click);
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm4::button2_Click);
 			// 
-			// MyForm3
+			// dataGridView1
+			// 
+			this->dataGridView1->AllowUserToDeleteRows = false;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Columns->AddRange(gcnew cli::array< System::Windows::Forms::DataGridViewColumn^  >(2) {
+				this->PersonID,
+					this->name
+			});
+			this->dataGridView1->Location = System::Drawing::Point(224, 160);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(286, 235);
+			this->dataGridView1->TabIndex = 18;
+			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm4::dataGridView1_CellContentClick_1);
+			// 
+			// PersonID
+			// 
+			this->PersonID->HeaderText = L"PersonID";
+			this->PersonID->Name = L"PersonID";
+			this->PersonID->ReadOnly = true;
+			// 
+			// name
+			// 
+			this->name->HeaderText = L"name";
+			this->name->Name = L"name";
+			// 
+			// MyForm4
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::SystemColors::ButtonShadow;
-			this->ClientSize = System::Drawing::Size(538, 394);
+			this->ClientSize = System::Drawing::Size(538, 456);
+			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->button1);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->panel1);
-			this->Name = L"MyForm3";
-			this->Text = L"MyForm3";
+			this->Name = L"MyForm4";
+			this->Text = L"MyForm4";
 			this->panel1->ResumeLayout(false);
 			this->panel1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
 			this->PerformLayout();
 
 		}
 #pragma endregion
-	private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+	private: System::Void tabPage1_Click(System::Object^ sender, System::EventArgs^ e) {
 	}
-	private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
+private: System::Void label2_Click(System::Object^ sender, System::EventArgs^ e) {
+}
 	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 		sqlite3* db;
 		char* errorMessage = nullptr;
@@ -171,36 +224,48 @@ namespace Project1 {
 		}
 		else {
 			MessageBox::Show("База данных успешно открыта!");
-			String^ personID = textBox1->Text;
-			// Получаем значение поля PersonID из текстового поля
 
-			// Преобразуем значение PersonID в строку в формате ANSI
-			char* charPersonID = static_cast<char*>(Marshal::StringToHGlobalAnsi(personID).ToPointer());
-
-			// Формируем SQL-запрос для удаления строки
-			String^ query = "DELETE FROM Person WHERE PersonID = '" + personID + "';";
+			String^ query = "SELECT * FROM Person;";
 			std::string queryStr = msclr::interop::marshal_as<std::string>(query);
 
 			// Выполняем SQL-запрос
-			rc = sqlite3_exec(db, queryStr.c_str(), NULL, 0, &errorMessage);
+			sqlite3_stmt* stmt;
+			int rc = sqlite3_prepare_v2(db, queryStr.c_str(), -1, &stmt, NULL);
 
 			// Обрабатываем результат выполнения запроса
 			if (rc != SQLITE_OK) {
-				MessageBox::Show("Ошибка при выполнении SQL-запроса!");
+				MessageBox::Show("Ошибка при подготовке SQL-запроса!");
 			}
 			else {
-				MessageBox::Show("Строка успешно удалена из базы данных!");
+				// Итерируемся по результатам запроса
+				while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+					// Получаем значения из текущей строки
+					String^ personID = gcnew String((char*)sqlite3_column_text(stmt, 0));
+					String^ name = gcnew String((char*)sqlite3_column_text(stmt, 1));
+					String^ surname = gcnew String((char*)sqlite3_column_text(stmt, 2));
+					String^ complect = gcnew String((char*)sqlite3_column_text(stmt, 3));
+					String^ fio = gcnew String((char*)sqlite3_column_text(stmt, 4));
+					String^ sum = gcnew String((char*)sqlite3_column_text(stmt, 5));
+
+					// Добавляем новую строку в таблицу
+					dataGridView1->Rows->Add(personID, name, surname, complect, fio, sum);
+				}
+
+				// Очищаем подготовленный запрос
+				sqlite3_finalize(stmt);
 			}
 
 			// ...
-
-			// Очищаем выделенную память
-			Marshal::FreeHGlobal(IntPtr(charPersonID));
 		}
-	}
+	};
 	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
 		Owner->Show();
 		this->Close();
 	}
+private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+}
+private: System::Void dataGridView1_CellContentClick_1(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) {
+}
 };
 }
+
